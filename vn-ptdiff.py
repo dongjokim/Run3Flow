e37a61d7ae33fb49232ad01a43ecd8e11259452d
DongJo Kim <dong.jo.kim@cern.ch>
2024-08-26 14:11:24 +0300

    new bins

--- vn-ptdiff.py
+++ vn-ptdiff.py
@@ -17,25 +17,24 @@ plotParams = {
         #"jyu_hydro":{"plotType":"theory","color":"orange","linestyle":"--","alpha":0.7,"label":"{T\\raisebox{-.5ex}{R}ENTo}+VISH(2+1)+UrQMD"},
 };
 
-cent =    [0,5,10,15,20,25,30,35,40,45,50,60,65,70,75,80];
-icHEP =   [0,1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9, 9];
+cent =    [0, 5, 10, 20, 30,40, 50, 60, 80];
 
-plot = JPyPlotRatio.JPyPlotRatio(panels=(3,3), #number of panels (each with ratio)
+plot = JPyPlotRatio.JPyPlotRatio(panels=(3,2), #number of panels (each with ratio)
 	xlabel="$p_\\mathrm{T}$", #create a 3x3 grid plot
 	panelLabel={i: "{}--{} %".format(cent[i],cent[i+1]) for i in range(0,9)}, #label the panels v_n
 	#panelScaling={1:2.0,2:3.0}, #add scaling to some of the panels (panel index : scale factor)
 	systPatchWidth=0.08,
-    ratioBounds ={0:(0.5,1.5),1:(0.1,2),2:(0.1,7)},
+    ratioBounds ={0:(0.5,1.5),1:(0.1,2)},
 	ratioSystPlot=True,
 	ylabel="$v_2$",disableRatio=[1,2]);
 
 plots = {};
 gr_vn = {};
-for ic in range(0,9):
+for ic in range(0,6):
         #print("gr_v{}".format(i+2));
-	if(ic<6):
-		gr_vn[(0,ic)] = data["Run2"].Get("Table 2/Graph1D_y{}".format(icHEP[ic]))
-		plots[0] = plot.Add(ic,gr_vn[(0,ic)],**plotParams["Run2"]);
+	
+	gr_vn[(0,ic)] = data["Run2"].Get("Table 2/Graph1D_y{}".format(ic));
+	plots[0] = plot.Add(ic,gr_vn[(0,ic)],**plotParams["Run2"]);
 	gr_vn[(1,ic)] = data["Run3"].Get("gPt_{}".format(ic));
 	plots[1] = plot.Add(ic,gr_vn[(1,ic)],**plotParams["Run3"]);
 	#plot.Ratio(plots["Run3"],plots["Run2"]);
